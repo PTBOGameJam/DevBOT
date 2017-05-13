4e471f91c9c1f44d0f5f0e0de4eed81730f029d9
Matthew Davey <reapazor@gmail.com>
2017-05-13 13:40:38 -0400

    Interesting the embedded just doesn’t want to send

--- dispatcher.go
+++ dispatcher.go
@@ -17,10 +17,12 @@ func messageHandler(session *discordgo.Session, message *discordgo.MessageCreate
 	contentSplit := strings.Split(message.Content, ",")
 	command := contentSplit[0]
 
-	// Check and Dispatch
+	// Check and Dispatch!
 	switch command {
 	case ".ping":
 		handlePing(session, message)
+	case ".meetup":
+		handleMeetup(session, message)
 	default:
 		return
 	}
